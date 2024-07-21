45ef6daca7ec42af5a7c6ddfc7d69a5cb7baf1c1
Madesh-k <madesk.kcse@gmail.com>
2024-07-21 18:01:08 +0530

    first commit

---
+++ vite-project/components/Card.jsx
@@ -0,0 +1,26 @@
+export default function Card(props) {
+  let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+  return(
+    <div className="card">
+      { badgeText && <div className="card-badge">{badgeText}</div>}
+        <img src={`../images/${props.coverImg}`} 
+             className="card-image"
+             alt="Main card image."/>
+        <div className="card-status">
+        <img src="../images/star.png" 
+             className="card-star"
+             alt="Star icon."/>
+                <span >{props.stats.rating}</span>
+                <span className="gray">{`(${props.stats.reviewCount})`} • </span>
+                <span className="gray">{props.location}</span>
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-price"> <span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    </div>
+  )
+}    
---
+++ vite-project/components/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return(
+        <section className="hero">
+          <img src="../images/photo-grid.png" className="photo-grid"></img>
+          <h1 className="title">Online Experiences</h1>
+          <p className="text">Join unique interactive skills let by 
+            one-of-a-kind hosts-all without leaving home
+          </p>
+        </section>
+    )
+}
